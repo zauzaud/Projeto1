0e47fa5eb034463700cb5b459f41ccd947046161
Zaude <Zaude@Zaudera>
2022-03-21 22:52:13 -0300

    Exercicio 2 - meio feito exercicio 3 - totalmente feito

--- Projeto1LabProgramacao/src/Exercicio2.java
+++ Projeto1LabProgramacao/src/Exercicio2.java
@@ -1,10 +1,27 @@
+import java.util.Random;
 
 public class Exercicio2 {
 
 	public static void main(String[] args) {
 		
+		Random gerador = new Random();
 		
+		int matriz [][] = new int [4][4]; 
+		
+		for (int linha = 0; linha < matriz.length; linha++) {
+			for(int coluna = 0; coluna <matriz.length; coluna++) {
+				matriz[linha][coluna] = gerador.nextInt(20);
+			}	
+		}
+		for (int linha = 0; linha < matriz.length; linha++) {
+			for(int coluna = 0; coluna <matriz.length; coluna++) {
+				System.out.print(matriz[linha][coluna] + "\t");
+				
+	  }	
+			
+		System.out.println();
 
 	}
+}
 
 }
--- Projeto1LabProgramacao/src/Exercicio3.java
+++ Projeto1LabProgramacao/src/Exercicio3.java
@@ -10,7 +10,7 @@ public class Exercicio3 {
 		
 		int nalunos;
 		
-		System.out.println("Informe o numero de alunos: ");
+		System.out.println("Informe o numero de estudantes: ");
 		nalunos = teclado.nextInt();
 		
 		char matriz [][] = new char [nalunos][10];
@@ -18,7 +18,7 @@ public class Exercicio3 {
 		int auxiliar [] = new int[10];
 		
 		 for (int linha = 0; linha < nalunos; linha++) {
-			 System.out.println("Resposta Aluno: " + (linha+1) + ":");
+			 System.out.println("Resposta Estudante: " + (linha+1) + ":");
 				for(int coluna = 0; coluna < matriz[linha].length; coluna++) {
 					auxiliar[coluna] = gerador.nextInt(5);
 					switch(auxiliar[coluna]) { 
@@ -44,11 +44,34 @@ public class Exercicio3 {
 				}
 		      System.out.println();
 		 }
-		 System.out.println(" ");
-		 for(int linha = 0 )
+		 System.out.println("Digite seu gabarito:  ");
+		 for(int linha = 0; linha<gabarito.length; linha++) {
+			 System.out.println("Gabarito: " + (linha+1) + ":");
+			 gabarito[linha] = teclado.next().charAt(0);
+		 }
+		 
+		 int acertos = 0;
+		 for (int linha = 0; linha<nalunos; linha++) { 
+			 for(int coluna = 0; coluna<matriz[linha].length; coluna++) {
+				 if(gabarito[coluna] == matriz[linha][coluna]) {
+					 acertos++;
+				 }
+			 }
+			 System.out.println("O estudante " + (linha+1) + " acertou: " + acertos);
+			 if (acertos>=7) { 
+				 System.out.println("O estudante " + (linha+1) + " foi aprovado com : " + acertos);
+			 } else { 
+				 System.out.println("O estudante " + (linha+1) + " foi reprovado.");
+			 }
+			acertos = 0;
+		 }
+		 
+		 
+			 
 	}
-
 }
+
+
 		 
 
 	
